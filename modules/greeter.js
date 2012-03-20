9b1ffe5b59476136937b0cb86b978a923b90ffd6
Johnny Sørensen <post@johsoe.dk>
2012-03-20 10:10:22 +0100

    chandata fix on pm, undefined if not on channel

--- modules/greeter.js
+++ modules/greeter.js
@@ -45,7 +45,7 @@ Greeter.prototype.setupListeners = function() {
 	});
 	
 	self.bot.on('pm', function(nick, message) {
-		if('string' == typeof self.bot.chanData( self.options.channel ).users[ nick ] ) {
+		if( self.bot.chanData( self.options.channel ) && 'string' == typeof self.bot.chanData( self.options.channel ).users[ nick ] ) {
 			
 			var cmd = message.substring(0, message.indexOf(" "));
 			
